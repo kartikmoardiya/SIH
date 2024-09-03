c6f01f4976f31b6d05bb8cbc9ffe0b9fb9f2977e
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2024-09-03 14:06:39 +0530

    scommit

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const mp4_to_text = require('./Routes/mp4_to_text')
 const generate_pdf = require('./generate_pdf')
 const user = require('./Routes/users')
 const pdf = require('./Routes/addPdf')
-const app = require('./backend/React-Pdf-Multer-Backend/app')
+const app = require('./backend/app')
 
 
 app.use(cors());
